f8d0a00fc3cad7555fcb6baba51b1fb7518ca431
Leon Jäkel <info@leon-jaekel.com>
2018-07-16 16:43:31 +0200

    Local notifications plugin integration using ionic framework documentation implented

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { LocalNotifications } from "@ionic-native/local-notifications";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(private localNotifications: LocalNotifications) {}
 
-  constructor(public navCtrl: NavController) {
+  onClick() {
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: "Hallo Phil!",
+      sound: null,
+      data: { secret: 'I am your personal secret' }
+    });
 
+    // Schedule multiple notifications
+    this.localNotifications.schedule([
+      {
+        id: 2,
+        text: "Servus Leon",
+        sound: null,
+        data: { secret: 'I am your personal secret' }
+      },
+      {
+        id: 3,
+        title: "Hoi Nils",
+        text: "Multi ILocalNotification 2",
+        data: { secret: 'I am your personal secret' }
+      }
+    ]);
+
+    // Schedule delayed notification
+    this.localNotifications.schedule({
+      text: "Sorry, ich bin zu spät!",
+      trigger: { at: new Date(new Date().getTime() + 3600) },
+      led: "FF0000",
+      sound: null
+    });
   }
-
 }
